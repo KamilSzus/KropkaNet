495866a7c3505b60cbe242160d33defa577aa3b4
KamilSzus <kamil-szustak1@wp.pl>
2022-03-26 20:56:50 +0100

    refactor tests

--- MyVector/MyVectorTest.cs
+++ MyVector/MyVectorTest.cs
@@ -9,38 +9,32 @@ namespace MyVector
 {
     class MyVectorTest
     {
-        [Test]
-        public void whenCreatingVectorValuesInClassShouldBeCorrect()
+        MyVector<int> vector;
+
+        [SetUp]
+        public void init()
         {
-            int expectedCappacity = 5;
-            int expectedSize = 0;
+            vector = new MyVector<int>(3);
+            vector.pushBack(1);
+            vector.pushBack(2);
+            vector.pushBack(3);
+        }
 
-            MyVector<int> vector = new MyVector<int>(expectedCappacity);
-
+        [Test,TestCase(3)]
+        public void whenCreatingVectorValuesInClassShouldBeCorrect(int expectedCappacity)
+        {
             Assert.AreEqual(expectedCappacity, vector.m_cappacity);
-            Assert.AreEqual(expectedSize, vector.m_size);
         }
 
         [Test,TestCase(new int[] { 1, 2, 3 })]
         public void whenAddingElementToEndOFVectorShouldContainsThisElement(int[] expectedValue)
         {
-            MyVector<int> vector = new MyVector<int>(3);
-
-            vector.pushBack(1);
-            vector.pushBack(2);
-            vector.pushBack(3);
-
             Assert.AreEqual(expectedValue[2], vector[2]);
         }
 
         [Test,TestCase(new int[] { 1, 2, 3, 4, 5 }, 6)]
         public void whenAddingElementToFullVectorCappacityShouldBeIncrease(int[] shouldContainsAtLeast , int expectedNewCappacity)
         {
-            MyVector<int> vector = new MyVector<int>(3);
-
-            vector.pushBack(1);
-            vector.pushBack(2);
-            vector.pushBack(3);
             vector.pushBack(4);
             vector.pushBack(5);
 
@@ -51,7 +45,6 @@ namespace MyVector
         [Test]
         public void whenCallIndexingOutOfArrayShouldThrowException()
         {
-            MyVector<int> vector = new MyVector<int>(3);
             Assert.That(() => vector[6],
             Throws.Exception
               .TypeOf<IndexOutOfRangeException>());
