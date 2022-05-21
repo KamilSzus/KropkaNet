d212eb64ecd9ea1ea7b12a5340d95815a44a6927
KamilSzus <69347040+KamilSzus@users.noreply.github.com>
2022-05-21 22:39:00 +0200

    thread

---
+++ MyVectorThread/MyVector.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace MyVector
+{
+    class MyVector<Type>
+    {
+        public delegate void EventHandler(int size);
+        public EventHandler eventHandler;
+        public EventHandler eventHandler1;
+
+        private object locking = new object();
+
+        private Type[] m_myVector;
+        public int Size//index ostatniego elementu
+        {
+            get;
+            private set;
+        }
+        //m_myVector.Length() ilosc zaalokowanej pamieci
+        public Type this[int index]
+        {
+            get
+            {
+                if (index >= Size || index <= -1)
+                {
+                    throw new IndexOutOfRangeException("Wartosc o podanym indeksie jest nieosiagalna");
+                }
+                return m_myVector[index];
+            }
+            set
+            {
+                if (index <= -1)
+                {
+                    throw new IndexOutOfRangeException("Wartosc o podanym indeksie jest nieosiagalna");
+                }
+                if (index >= m_myVector.Length)
+                {
+                    allocate((index + 1) * 2);
+                    if (eventHandler != null)
+                    {
+                        eventHandler1(m_myVector.Length);
+                    }
+                }
+                m_myVector[index] = value;
+                if (index >= Size)
+                {
+                    Size = index + 1;
+                    if (eventHandler != null)
+                    {
+                        eventHandler(Size);
+                    }
+                }
+            }
+        }
+
+        public MyVector(int cappacityOfNewVector)
+        {
+            m_myVector = new Type[cappacityOfNewVector];
+            Size = 0;
+        }
+
+        public void pushBack(Type value)
+        {
+            this[Size] = value;
+        }
+
+        public void pushBackBlocking(Type value)
+        {
+            lock (locking)
+            {
+                this[Size] = value;
+            }
+        }
+
+        public void saveToFile(string fileName)
+        {
+            using (StreamWriter fs = File.CreateText(fileName))
+            {
+                for (int i = 0; i < Size; i++)
+                    fs.WriteLine(this[i].ToString());
+            }
+        }
+
+        public Boolean pushBackNonBlocking(Type value)
+        {
+            if (Monitor.TryEnter(locking, 0))
+            {
+                try
+                {
+                    this[Size] = value;
+                }
+                finally
+                {
+                    Monitor.Exit(locking);
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void allocate(int newCappacity)
+        {
+            Array.Resize(ref m_myVector, newCappacity);
+        }
+
+        public void dump()
+        {
+            Console.WriteLine("Allocated memory: " + m_myVector.Length);
+            Console.WriteLine("Last index: " + Size);
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write(m_myVector[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        protected virtual void OnEventHandler(int size)
+        {
+            eventHandler?.Invoke(size);
+        }
+
+        protected virtual void OnEventHandler1(int size)
+        {
+            eventHandler1?.Invoke(size);
+        }
+    }
+}
---
+++ MyVectorThread/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyVector
+{
+    class Program
+    {
+        private static MyVector<int> safeThreadMyVector = new MyVector<int>(0);
+        readonly private static int threadNumber = 64;
+        static void PrintSize(int size)
+        {
+            Console.WriteLine("Size = {0}", size);
+        }
+
+        static void PrintCapacity(int size)
+        {
+            Console.WriteLine("Capacity = {0}", size);
+        }
+
+        static private void BlockingAdd()
+        {
+            Stopwatch sw = new Stopwatch();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            sw.Start();
+            safeThreadMyVector.pushBackBlocking(threadId);
+            sw.Stop();
+
+            Console.WriteLine($"Czas oczekiwania {threadId}: {sw.Elapsed}");
+        }
+
+        static private void NonblockingAdd()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            bool isAdded = safeThreadMyVector.pushBackNonBlocking(threadId);
+
+            Console.WriteLine(isAdded ? $"Dodano id dla wątku {threadId}" : $"Niedodano id dla wątku {threadId}");
+        }
+
+        static void Main(string[] args)
+        {
+
+            Thread[] threads = new Thread[threadNumber];
+            Random random = new Random();
+//            safeThreadMyVector.eventHandler += PrintSize;
+//            safeThreadMyVector.eventHandler1 += PrintCapacity;
+
+            for (int i = 0; i < threadNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    var thread = new Thread(BlockingAdd);
+                    threads[i] = thread;
+                    thread.Start();
+                }
+                else
+                {
+                    var thread = new Thread(NonblockingAdd);
+                    threads[i] = thread;
+                    thread.Start();
+                }
+            }
+
+            for (int i = 0; i < threadNumber; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine("Success");
+
+            safeThreadMyVector.saveToFile("array.txt");
+
+            Console.ReadKey();
+        }
+    }
+}
